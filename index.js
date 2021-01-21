5e4b9b3e3b9ba803b845041ef88ca8f6b7a286dc
Esiena Ekwofia <eekwofia@gmail.com>
2021-01-20 21:37:17 -0500

    created list of questions

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
 
-function init(){
-    inquirer
-      .prompt([
-        {
+function init()
+inquirer
+.prompt([
+    {
+        type: "input",
+        message: "What is your name?", 
+        name: "employeename",
+    }, 
+    {
+        type: "input", 
+        message: "Enter you ID number.",
+        name: "id", 
+    }, 
+    {
+        type: "input", 
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        // Only ask the manager 
+        type: "input", 
+        message: "What is your office number?",
+        name: "number",
+    },
+    {
+        type: "list", 
+        message: "What is your role?",
+        name: "role",
+        choices: ['Manager', 'Engineer', 'Intern']
+    },
+    {
+        //only ask Engineer 
+        type: "input", 
+        message: "What is your GitHub?",
+        name: "github",
+    },
+    {
+        // only ask Intern
+        type: "input", 
+        message: "What school do you attend?",
+        name: "school",
+    },
+
+])
+.then ((answers))
