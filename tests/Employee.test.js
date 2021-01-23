0190d5e46e6642eb310b4f551f83f22a2f45b874
Esiena Ekwofia <eekwofia@gmail.com>
2021-01-22 23:33:16 -0500

    pesudocode example of test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,29 @@
 const Employee = require("../lib/Employee");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
-const Manager = require("../lib/Manager");
 
+describe('Employee', () => {
+    describe('Initialization', () => {
+      // Positive test
+      it("should create an object with a 'text' property set to the 'text' argument provided when called with the 'new' keyword", () => {
+        // Arrange
+        const text = 'Pick up milk';
+  
+        // Act
+        const obj = new Todo(text);
+  
+        // Assert
+        expect(obj.text).toEqual(text);
+      });
+  
+      // Exception test
+      it("should throw an error if not provided a 'text' value", () => {
+        // Arrange
+        const cb = () => new Todo();
+        const err = new Error(
+          "Expected parameter 'text' to be a non empty string"
+        );
+  
+        // Assert
+        expect(cb).toThrowError(err);
+      });
+    });
+  });
--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,4 +1,26 @@
-const Employee = require("../lib/Employee");
-const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/Intern");
-const Manager = require("../lib/Manager");
+
+
+describe('getSchool', () => {
+    describe("Initialization", () => {
+it ("This will set the school of the intern via the constructor", () => {
+    const testSchool = "Columbia";
+    const a = new School("Foo", 64, "Columbia@gmail.com", testSchool);
+    expect(a.school).toBe(testSchool);
+    console.log(a.school)
+})
+})
+
+
+it("should throw an error if not provided a 'text' value", () => {
+    // Arrange
+    const cb = () => new getSchool();
+    const err = new Error(
+      "Expected parameter 'text' to be a non empty string"
+    );
+
+    // Assert
+    expect(cb).toThrowError(err);
+ })
+
+  });
